b0cd9bd4cddfa2c64a5c54000c012c50a8c28450
vitor-chioda <vitor.chioda@gmail.com>
2022-03-18 21:42:49 -0300

    Configurações iniciais

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
     <router-view/>
+    <div class="cover"></div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*:not(i) {
+  font-family: 'Open Sans' ;
+  color: #fff ;
 }
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+body 
+{
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+background-image: url("@/assets/img/background.png");
+background-size:cover;
+background-repeat: no-repeat;
+min-height: 100vh;
+min-width: 320px;
+max-width: 1920px;
+margin: 0 auto;
+
+
+.cover{
+  position:fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background:rgba(34,34,34,.9);
+  z-index: -1;
+}
+
+
 }
 </style>
